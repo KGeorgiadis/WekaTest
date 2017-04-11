485d37859d7641030deb64f7cbce94c5b8727bc6
Konstantinos Georgiadis <gkn@pwo.ou.nl>
2017-04-11 09:34:43 +0200

    no message

--- WekaTest/Program.cs
+++ WekaTest/Program.cs
@@ -5,7 +5,13 @@ using System.Text;
 using System.Threading.Tasks;
 using ikvm.runtime;
 using weka;
-
+using weka.clusterers;
+using weka.core.converters;
+using java.io;
+using weka.core;
+using weka.classifiers.meta;
+using weka.attributeSelection;
+using weka.classifiers.evaluation;
 
 namespace WekaTest
 {
@@ -14,10 +20,9 @@ namespace WekaTest
 
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello Java, from C#!");
-            //classifyTest();
-            BayesTest();
-            Console.ReadKey();
+            //BayesTest();
+            DensityBasedClusterer();
+            System.Console.ReadKey();
         }
 
         const int percentSplit = 66;
@@ -29,9 +34,8 @@ namespace WekaTest
                 weka.core.Instances insts = new weka.core.Instances(new java.io.FileReader("iris.arff"));
                 insts.setClassIndex(insts.numAttributes() - 1);
 
-                //weka.classifiers.Classifier cl = new weka.classifiers.trees.J48();
                 weka.classifiers.Classifier cl = new weka.classifiers.bayes.BayesNet();
-                Console.WriteLine("Performing " + percentSplit + "% split evaluation.");
+                System.Console.WriteLine("Performing " + percentSplit + "% split evaluation.");
 
                 //randomize the order of the instances in the dataset.
                 weka.filters.Filter myRandom = new weka.filters.unsupervised.instance.Randomize();
@@ -46,7 +50,7 @@ namespace WekaTest
 
                 cl.buildClassifier(train);
                 //print model
-                Console.WriteLine(cl);
+                System.Console.WriteLine(cl);
 
                 int numCorrect = 0;
                 for (int i = trainSize; i < insts.numInstances(); i++)
@@ -59,14 +63,14 @@ namespace WekaTest
 
                     for (int x = 0; x < prediction.Length; x++)
                     {
-                        Console.WriteLine("Probability of class [{0}] for [{1}] is: {2}", currentInst.classAttribute().value(x), currentInst, Math.Round(prediction[x],4));
+                        System.Console.WriteLine("Probability of class [{0}] for [{1}] is: {2}", currentInst.classAttribute().value(x), currentInst, Math.Round(prediction[x],4));
                     }
-                    Console.WriteLine();
+                    System.Console.WriteLine();
                     
                     if (predictedClass == insts.instance(i).classValue())
                         numCorrect++;
                 }
-                Console.WriteLine(numCorrect + " out of " + testSize + " correct (" +
+                System.Console.WriteLine(numCorrect + " out of " + testSize + " correct (" +
                             (double)((double)numCorrect / (double)testSize * 100.0) + "%)");
 
                 // Train the model
@@ -76,13 +80,13 @@ namespace WekaTest
                 // Print the results as in Weka explorer:
                 //Print statistics
                 String strSummaryTrain = eTrain.toSummaryString();
-                Console.WriteLine(strSummaryTrain);
+                System.Console.WriteLine(strSummaryTrain);
 
                 //Print detailed class statistics
-                Console.WriteLine(eTrain.toClassDetailsString());
+                System.Console.WriteLine(eTrain.toClassDetailsString());
 
                 //Print confusion matrix
-                Console.WriteLine(eTrain.toMatrixString());
+                System.Console.WriteLine(eTrain.toMatrixString());
 
                 // Get the confusion matrix
                 double[][] cmMatrixTrain = eTrain.confusionMatrix();
@@ -95,23 +99,81 @@ namespace WekaTest
                 // Print the results as in Weka explorer:
                 //Print statistics
                 String strSummaryTest = eTest.toSummaryString();
-                Console.WriteLine(strSummaryTest);
+                System.Console.WriteLine(strSummaryTest);
 
                 //Print detailed class statistics
-                Console.WriteLine(eTest.toClassDetailsString());
+                System.Console.WriteLine(eTest.toClassDetailsString());
 
                 //Print confusion matrix
-                Console.WriteLine(eTest.toMatrixString());
+                System.Console.WriteLine(eTest.toMatrixString());
 
                 // Get the confusion matrix
                 double[][] cmMatrixTest = eTest.confusionMatrix();
 
             }
+
             catch (java.lang.Exception ex)
             {
                 ex.printStackTrace();
             }
 
+        }
+
+        public static void DensityBasedClusterer ()
+        {
+            try
+            {
+                Instances data = new Instances(new java.io.FileReader("politeness.arff"));
+
+                MakeDensityBasedClusterer clusterer = new MakeDensityBasedClusterer();
+               
+                // set further options for EM, if necessary...                
+                clusterer.setNumClusters(3);
+                clusterer.buildClusterer(data);
+
+                ClusterEvaluation eval = new ClusterEvaluation();
+                eval.setClusterer(clusterer);
+                eval.evaluateClusterer(data);
+
+
+                /** Print Prior probabilities for each cluster
+                *double[] Priors = clusterer.clusterPriors();
+                *
+                *for(int x=0; x<Priors.Length; x++)
+                *{
+                *   System.Console.WriteLine(Priors[x]);
+                *}
+                **/
+
+                /**Print default capabilities of the clusterer (i.e., of the wrapper clusterer).
+                *Capabilities Capa = clusterer.getCapabilities();
+                *System.Console.WriteLine(Capa);
+                **/
+
+                /**Print the current settings of the clusterer.
+                *String[] Opts = clusterer.getOptions();
+                *for (int x = 0; x < Opts.Length; x++)
+                *{
+                *    System.Console.WriteLine(Opts[x]);
+                *}
+                **/
+
+                //string gInfo = clusterer.globalInfo();
+                //System.Console.WriteLine(gInfo);
+
+                java.util.Enumeration enumOpts = clusterer.listOptions();
+                System.Console.WriteLine(enumOpts);
+
+                //Print all results for clusterer as in Weka
+                //System.Console.WriteLine(eval.clusterResultsToString());
+
+
+            }
+
+            catch (java.lang.Exception ex)
+            {
+                ex.printStackTrace();
+            }
         }
     }
 }
